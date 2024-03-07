a032fffe2d6eef4cd5551eda7390637d8c35deb4
Yulia <lutsenkoyulia2004@gmail.com>
2024-03-07 15:11:37 +0200

    changed config file

--- src/config/connection.js
+++ src/config/connection.js
@@ -1,10 +1,19 @@
 require("dotenv").config();
 const { Pool } = require("pg");
-const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_DATABASE,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
-});
+
+let pool;
+
+if (process.env.DB_URL) {
+  pool = new Pool({
+    connectionString: process.env.DB_URL,
+  });
+} else {
+  pool = new Pool({
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_DATABASE,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT,
+  });
+}
 module.exports = pool;
